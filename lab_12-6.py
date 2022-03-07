7605237fef639dcdba864e2d974af6a3db1bc11f
P22inolan <p22inolan@fairfieldprep.org>
2022-03-07 10:14:48 -0500

    fix 3/7

--- lab_12-6.py
+++ lab_12-6.py
@@ -1,14 +1,14 @@
 # Author: IBN (AMDG) 3/1/2022
-grades = {100:"end_s1_labs", 100:"end_s2_labs", 0:"cycle_10_labs", 100:"mid_year_project_proposal", 100:"cycle_10_practice_quiz", 80:"cycle_10_quiz"}
+grades = {100: "end_s1_labs", 100: "end_s2_labs", 0: "cycle_10_labs", 100: "mid_year_project_proposal", 100: "cycle_10_practice_quiz", 80: "cycle_10_quiz"}
 print(grades.keys())
 
-for (k,v) in grades.items():
+for (k, v) in grades.items():
     print(k, v)
 
-for (k,v) in grades.items():
+for (k, v) in grades.items():
     if k > 70:
         print(k, v)
 
-for (k,v) in grades.items():
+for (k, v) in grades.items():
     if k < 50:
         print(k, v)
